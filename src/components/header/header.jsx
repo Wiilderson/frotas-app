3215fb6afa68ddcd4a2bf0ea7d77b7ff022b23e5
wilderson <wilderson@uitecnologia.com.br>
2024-07-13 14:53:23 -0300

    alteração nos links do header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 function Header() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const menuItems = [
+    { name: 'Entregas', path: '/' },
+    { name: 'Caminhões', path: '/caminhoes' },
+    { name: 'Motoristas', path: '/motoristas' },
+  ];
   return (
     <header>
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
@@ -10,30 +18,21 @@ function Header() {
             id="mobile-menu-2"
           >
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-              <li>
-                <Link
-                  to="/"
-                  className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                >
-                  Entregas
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/caminhoes"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Caminhões
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to="/motoristas"
-                  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  Motoristas
-                </Link>
-              </li>
+              {menuItems.map((item, index) => (
+                <li key={index}>
+                  <Link
+                    to={item.path}
+                    className={`block py-2 pr-4 pl-3 rounded lg:p-0 ${
+                      activeIndex === index
+                        ? 'text-white bg-primary-700 lg:bg-transparent lg:text-primary-700 dark:text-white'
+                        : 'text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700'
+                    }`}
+                    onClick={() => setActiveIndex(index)}
+                  >
+                    {item.name}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
